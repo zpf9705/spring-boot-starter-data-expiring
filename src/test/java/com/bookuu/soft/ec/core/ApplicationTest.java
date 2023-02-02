bcb6f69fbb4d296fec3f5b033ca68fea5fc0cccf
feifei <929160069@qq.com>
2023-02-02 17:56:45 +0800

    add project

--- src/test/java/com/bookuu/soft/ec/core/ApplicationTest.java
+++ src/test/java/com/bookuu/soft/ec/core/ApplicationTest.java
@@ -1,9 +1,7 @@
 package com.bookuu.soft.ec.core;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ApplicationTest {
 
     @Test
