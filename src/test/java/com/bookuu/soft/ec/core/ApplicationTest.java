a72094be044805f6e426e37c1102ac3d1cb72306
feifei <929160069@qq.com>
2023-02-02 17:56:27 +0800

    add project

---
+++ src/test/java/com/bookuu/soft/ec/core/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.bookuu.soft.ec.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void setApplicationContext() {
+    }
+}
