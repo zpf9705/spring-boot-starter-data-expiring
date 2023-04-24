fc00cd89af2606001ef4ff62df65a2b2250d558f
feifei <929160069@qq.com>
2023-04-24 11:22:24 +0800

    update：Utils

---
+++ src/main/java/io/github/zpf9705/expiring/util/ArrayUtils.java
@@ -0,0 +1,32 @@
+package io.github.zpf9705.expiring.util;
+
+import io.github.zpf9705.expiring.core.annotation.CanNull;
+
+/**
+ * @author zpf
+ * @since 3.0.0
+ */
+public class ArrayUtils {
+
+    /**
+     * Is Empty for {@code Array}
+     *
+     * @param array Check array
+     * @param <T>   {@code Object}
+     * @return if {@code true} Assertions by
+     */
+    public static <T> boolean simpleIsEmpty(@CanNull T[] array) {
+        return !simpleNotEmpty(array);
+    }
+
+    /**
+     * Is not Empty for {@code Array}
+     *
+     * @param array Check array
+     * @param <T>   {@code Object}
+     * @return if {@code true} Assertions by
+     */
+    public static <T> boolean simpleNotEmpty(@CanNull T[] array) {
+        return null != array && !(array.length == 0);
+    }
+}
