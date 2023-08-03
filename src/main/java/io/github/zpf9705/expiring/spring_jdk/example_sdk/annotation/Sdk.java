13ae9b31d2a5049e99212ec6a97c8801496b6454
zhangpengfei <929160069@qq.com>
2023-08-03 13:34:00 +0800

    update：update client any

--- src/main/java/io/github/zpf9705/expiring/spring_jdk/example_sdk/annotation/Sdk.java
+++ src/main/java/io/github/zpf9705/expiring/spring_jdk/example_sdk/annotation/Sdk.java
@@ -3,7 +3,12 @@ package io.github.zpf9705.expiring.spring_jdk.example_sdk.annotation;
 import java.lang.annotation.*;
 
 /**
- * SDK identification annotation
+ * Dynamic injection of Spring container annotations, which only need to be annotated on the interface class
+ * that needs dynamic injection, can be scanned by {@link SdkProxyBeanDefinitionRegister} and automatically
+ * create proxy objects based on annotation properties .
+ * <p>
+ * The class that wears this annotation can be injected and used in the container environment of the spring boot
+ * project through {@link org.springframework.beans.factory.annotation.Autowired}, constructors, or set methods
  *
  * @author zpf
  * @since 3.1.0
